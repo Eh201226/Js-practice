e7be769282e3fb4f86b564c24de79ccbd21c1ce2
Eh201226 <elduhade@gmail.com>
2025-07-27 15:37:37 +0000

    about variable

---
+++ src/D1/variable.js
@@ -0,0 +1,21 @@
+let firstName = "Nathan"; //               v
+let lastName = "Desalegn";
+let fatherName = "Habtamu";
+let motherName = "Addis";
+let sisterName = "Eldana";
+let age = 11;
+let country = "Italy";
+let city = "venezia";
+let bornIn = "Addis Abeba";
+
+const gravity = 9.14;
+const boilingPoint = 100;
+const PI = 3.14;
+
+let name = "Nathan";
+let job = "student";
+let live = "Marghera";
+
+console.log(firstName, lastName, country, city, age);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(name, job, live);
