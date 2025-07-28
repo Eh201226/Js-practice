ee185850b7997c2853ab1f0817c5d993f17893ce
Eh201226 <elduhade@gmail.com>
2025-07-28 19:35:05 +0000

    add number data types

---
+++ src/D2/number.js
@@ -0,0 +1,11 @@
+let age = 11
+const gravity = 9.90
+let mass = 30
+const PI = 3.14
+
+
+const boilingPoint = 100
+const bodyTemp = 37
+console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
+
+
