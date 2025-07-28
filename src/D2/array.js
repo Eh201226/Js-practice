22f9da0660e4cb5a09f28c610f6ac564bce7dade
Eh201226 <elduhade@gmail.com>
2025-07-28 20:24:13 +0000

    new string and array.js

---
+++ src/D2/array.js
@@ -0,0 +1,11 @@
+let num = [1, 2, 3];
+let str = ['a', 'b', 'c'];
+num.push(4);
+num.push(5);
+num.push(6);
+num.push(7);
+
+console.log(num)
+console.log(num.length)
+console.log(num[5]);
+
---
+++ src/D2/string.js
@@ -0,0 +1,15 @@
+let space = ' '
+let firstName = "Nathan"
+let lastName = "Desalegn"
+let motherName = "Addis"
+let middleName = "Habtamu"
+let sisterName = "Eldana"
+let age = 11
+let bornIn = "Addis Abeba, Ethiopia"
+let countryLive = "Venezia, Italy"
+let job = "student"
+
+let fullName = firstName + space + lastName
+console.log(fullName)
+
+console.log(firstName, space, middleName, lastName, motherName, sisterName, age, bornIn, countryLive, job)
