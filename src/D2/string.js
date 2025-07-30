2cc72924d0889f7b4bc8e385a16cf28d22a3fdb5
Eh201226 <elduhade@gmail.com>
2025-07-30 20:02:57 +0000

    update string

--- src/D2/string.js
+++ src/D2/string.js
@@ -10,9 +10,9 @@ let countryLive = "Venezia, Italy"
 let job = "student"
 
 let fullName = firstName + space + lastName
-console.log(fullName)
+console.log(fullName) // "Nathan Desalegn" // Concatenation using + operator and space variable
 
-console.log(firstName, space, middleName, lastName, motherName, sisterName, age, bornIn, countryLive, job)
+console.log(firstName, space, middleName, lastName, motherName, sisterName, age, bornIn, countryLive, job) // this is so good and simple to use console.log to print multiple variables at once
 
 let greeting = "Hi"
 let name = 'Nathan'
@@ -22,4 +22,9 @@ console.log( message)
   lastName = "Desalegn"
    fullName = firstName + " " + lastName
 
-console.log(fullName); // "Nathan Desalegn"
+console.log(fullName); // "Nathan Desalegn" // Concatenation using + operator // This is a simple way to concatenate strings in JavaScript.
+let chat = "Hello";
+chat += ", ";
+chat += "world!";
+console.log(chat); // "Hello, world!" // Concatenation using += operator
+// i didn't know this opreator before, but it is very useful for concatenating strings in a more readable way.  
