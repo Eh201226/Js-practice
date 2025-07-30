6418c8d0d90a684088e6843ae5d851cb811db9c3
Eh201226 <elduhade@gmail.com>
2025-07-30 20:45:44 +0000

    now README

--- src/D2/array.js
+++ src/D2/array.js
@@ -9,3 +9,6 @@ console.log(num)
 console.log(num.length)
 console.log(num[5]);
 
+let people = [{name: "Nathan"}, {name: "Addu"}];
+console.log(people[0].name); // "Nathan"
+console.log(people[1].name); // "Addu"
