2fd42aba7122ea48896ef40b68689baac35404d7
Eh201226 <elduhade@gmail.com>
2025-08-02 13:49:04 +0000

    comment

--- src 1/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('change-text-btn').addEventListener('click', function() {
-  const aboutText = document.getElementById('about-text');
-  aboutText.textContent = 'Thank you for visiting our website! We hope you find what you are looking for.';
-});
